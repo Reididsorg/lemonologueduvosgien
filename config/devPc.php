d453312d0819dad0bc13767bcc58ac6d6692af3f
Reididsorg <bruno.grosdidier@gmail.com>
2021-01-03 16:06:24 +0100

    deleted all files to put on gitignore

--- config/devPc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const HOST = 'localhost:3308';
-const DB_NAME = 'lemonologueduvosgien';
-const CHARSET = 'utf8';
-const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
-const DB_USER = 'lemonologueduvosgien';
-const DB_PASS = 'Vosgica88';
