8dadc18769d10c1ddf11db551b864f78828ccc59
Reididsorg <bruno.grosdidier@gmail.com>
2021-01-12 15:12:08 +0100

    first change

--- config/Request.php
+++ config/Request.php
@@ -10,7 +10,9 @@ class Request
 
     public function __construct()
     {
-        $this->sentByGet = new Parameter($_GET);
+        if(isset($_GET) && !empty($_GET)) {
+            $this->sentByGet = new Parameter($_GET);
+        }
         $this->sentByPost = new Parameter($_POST);
         $this->sentBySession = new Session($_SESSION);
     }
