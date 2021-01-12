4b7d5f0b3bded1cda588ac6300548015bbeb37b5
Reididsorg <bruno.grosdidier@gmail.com>
2021-01-12 13:36:17 +0100

    test

--- config/Session.php
+++ config/Session.php
@@ -15,10 +15,10 @@ class Session
     {
         $_SESSION[$name] = $value;
     }
-
+//filter_input(INPUT_POST, $name) instead of $_SESSION[$name]
     public function get($name)
     {
-        if (isset($_SESSION[$name])) {
+        if (null !== (filter_input(INPUT_POST, $name))) {
             return $_SESSION[$name];
         }
         return null;
